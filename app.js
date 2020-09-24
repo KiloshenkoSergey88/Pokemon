7f3ce73996974a790c601df61d89f0790b61d422
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-09-25 00:16:14 +0300

    0-15 25.09.2020

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import hello from './js/ptMarkup';
 
+
+hello();
 start.onclick = () => {
     document.getElementById("pageOne").style.display = "none";
     document.getElementById("pageTwo").style.display = "flex";
+    
 };
