49e4d459d77edb94452d6f764455330c45597a1f
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-09-27 16:52:16 +0300

    16 52 27.09.2020

--- js/ptMarkup.js
+++ js/ptMarkup.js
@@ -1,16 +1,21 @@
 const markup = document.getElementsByClassName('sidePt');
 
+
 function Card() {
   let fragment = new DocumentFragment();
 
-  for(let i = 0; i < 150; i++) {
+  for(let i = 0; i < 95; i++) {
     let card = document.createElement('sidePt');
     card.className = 'card';
-    card.textContent = 'I here';
-    fragment.append(card);
-  }
 
+    fragment.append(card);
+    
+  }
+  let img = ['./img/Bulbasaur.png','./img/C.png'].sort(()=>Math.random() - 0.5);
+	document.querySelectorAll('.card').forEach((el,i)=>el.insertAdjacentHTML('beforeend', `<img src="${img[i]}" height="80" width="80">`));
   return fragment;
 }
 
 markup[0].append(Card());
+markup[1].append(Card());
+console.log(document.querySelector('.card'));
