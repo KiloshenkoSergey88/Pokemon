7ecf0f75180c2d9b4720651b790d2422d5c468aa
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-09-27 11:31:25 +0300

    11 31 27 09 2020

--- js/ptMarkup.js
+++ js/ptMarkup.js
@@ -1,1 +1,16 @@
-console.log('111');
+const markup = document.getElementsByClassName('sidePt');
+
+function Card() {
+  let fragment = new DocumentFragment();
+
+  for(let i = 0; i < 150; i++) {
+    let card = document.createElement('sidePt');
+    card.className = 'card';
+    card.textContent = 'I here';
+    fragment.append(card);
+  }
+
+  return fragment;
+}
+
+markup[0].append(Card());
