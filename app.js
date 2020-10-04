cfb7dcf3ad1984be503ab34655fa2aae3cd4a944
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-10-04 12:14:10 +0300

    12 14 04.10.2020

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ start.onclick = () => {
     document.getElementById("pageOne").style.display = "none";
     document.getElementById("pageTwo").style.display = "block";
 };
-
+// скрипт для перехода со страницы выбора на страницу игрового поля.
 changeTwoPl.onclick = () => {
     document.getElementById("pageTwo").style.display = "none";
     document.getElementById("pageThree").style.display = "block";
-}
+};
 
