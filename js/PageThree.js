9a9dc97d929070740c6253348763c2613a3e0f8d
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-10-12 22:39:53 +0300

    22 39 12.10.2020

--- js/PageThree.js
+++ js/PageThree.js
@@ -7,6 +7,21 @@ fetch('../ServerApp/Data/DataChar.json')
     console.log('data: ', Obj);
 
     console.log(Obj.Pokemons);
+
+    let changeTwoPl = document.getElementById('changeTwoPl');
+    changeTwoPl.addEventListener('click', addDataPlayer);
+
+    function addDataPlayer() {
+        console.log('its worked');
+
+        
+        let appCharOneP1 = document.getElementById('addCharBattleP1').childNodes[1].id;
+        let appCharTwoP1 = document.getElementById('addCharBattleP1').childNodes[2].id;
+        let appCharThreeP1 = document.getElementById('addCharBattleP1').childNodes[3].id;
+        
+
+        console.log(appCharOneP1, appCharTwoP1,appCharThreeP1);
+    }
 });
 
 
