d54968ac40ea9e6162e065f4efb4caf677451d62
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-10-25 21:18:49 +0300

    21 18 25.10.2020

--- js/changeChar.js
+++ js/changeChar.js
@@ -1,18 +1,29 @@
-let topPanelPth1p = document.getElementById('topPanelPth1p'),
-    topPanelPth2p = document.getElementById('topPanelPth2p');
+let countHide = [0, 0];
 
-function hide1(e){
-
+function hide0(e){
   if (e.target.id != ('topPanelPth1p')) {
-    console.log(e.target.id);
+  
+    if (countHide[0] == 0) {
+
+      countHide[0]++;
+      console.log(countHide[0]);
+      pFPthGame1p.append(e.target);
+    }
   }
 };
-function hide2(e){
 
-    if (e.target.id != ('topPanelPth2p')) {
-      console.log(e.target.id);
+function hide1(e){
+  
+  if (e.target.id != ('topPanelPth2p')) {
+
+    if (countHide[1] == 0) {
+
+      countHide[1]++;
+      console.log(countHide[1]);
+      pFPthGame2p.append(e.target);
     }
+  }
 };
 
-topPanelPth1p.addEventListener('click', hide1, false);
-topPanelPth2p.addEventListener('click', hide2, false);
+topPanelPth1p.addEventListener('click', hide0, false);
+topPanelPth2p.addEventListener('click', hide1, false);
