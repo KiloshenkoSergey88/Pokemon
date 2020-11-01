91694b4b1a176762b6f1969278d22e7533402165
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2020-11-01 22:19:15 +0300

    22 18 01.11.2020

--- ServerApp/server.js
+++ ServerApp/server.js
@@ -1,26 +1,21 @@
-const express = require("express");
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const   express = require("express"),
+        app = express(),
+        http = require('http').Server(app),
+        io = require('socket.io')(http),
+        dataChar = require('./Data/DataChar.json');
 
 app.use("/", express.static("d:/JS/Pokemon"));
-
 io.on('connection', function(socket) {
     console.log('A user connected');
 
-    
+    socket.send(dataChar);
 
-     socket.on('clientEvent', function(data) {
-        console.log(data);
-        setTimeout(function() {
-            socket.send(`Сейчас я приду и угадаю это (${data})`);
-        }, 3000);
-    });
- 
+
+
     socket.on('disconnect', function () {
        console.log('A user disconnected');
     });
- });
+});
 
 http.listen(5000,"192.168.1.100", () => {
     console.log("Server running...")
